50afdf982198db86cf52581a5b3fd39d03ca5f2b
Akram Shaikh <114775373+Akram-Shk@users.noreply.github.com>
2022-10-01 01:41:27 +0530

    Add files via upload

---
+++ SolidRectange.java
@@ -0,0 +1,30 @@
+class  SolidRectange
+{
+	public static void main(String[] args) 
+	{
+        int n=4;
+		int m=5;
+
+         //outer loop
+		for(int i=1;i<=n;i++)
+			{  //inner loop
+		for(int j=1;j<=m;j++){
+			System.out.print("*");			
+		}
+		System.out.println();
+		}
+	}
+}
+
+
+
+/*
+    Using Nested Loop 
+
+           ->colums=5(m=5)       
+              *****
+ Rows =4(n=4) *****
+              *****
+              *****  
+
+*/
